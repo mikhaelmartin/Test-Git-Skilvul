b82c7beacd2324aa43789dc8015fa693faf0fce8
Mikhael Martin <mikhael.martin@gmail.com>
2023-03-21 10:44:09 +0700

    y on line 3

--- Progam.cs
+++ Progam.cs
@@ -2,6 +2,6 @@ void Main()
 {
     Console.WriteLine("line1");
     Console.WriteLine("line2");
-    Console.WriteLine("line3");
+    Console.WriteLine("line3y");
     Console.WriteLine("line4");
 }
